134bd9fb42ae28238aea7300ce0285bc1abe77b8
Muhammad Deril <49135753+derilkillms@users.noreply.github.com>
2023-03-08 11:48:43 +0700

    web

---
+++ handler_test.go
@@ -0,0 +1,22 @@
+package golang_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "<h1>Hello World</h1>")
+	}
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}

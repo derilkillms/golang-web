41490ff9626bcd2fb7dfd750665c4dc1834f6690
Muhammad Deril <49135753+derilkillms@users.noreply.github.com>
2023-03-13 15:11:28 +0700

    web

--- middleware_test.go
+++ middleware_test.go
@@ -10,10 +10,28 @@ type LogMiddleware struct {
 	Handler http.Handler
 }
 
+// mengimplementasikan interface Handler,
 func (middleware *LogMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("Before Execute Handler")
 	middleware.Handler.ServeHTTP(writer, request)
 	fmt.Println("After Execute Handler")
+}
+
+// membuat struct baru sebagai midleware ErrorHandler
+type ErrorHandler struct {
+	Handler http.Handler
+}
+
+func (errorHandler *ErrorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("Terjadi error")
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "Error %s", err)
+		}
+	}()
+	errorHandler.Handler.ServeHTTP(w, r)
 }
 
 func TestMiddleware(t *testing.T) {
@@ -22,13 +40,18 @@ func TestMiddleware(t *testing.T) {
 		fmt.Println("Handler Execulte")
 		fmt.Fprint(w, "Hello Middleware")
 	})
+
+	// middleware dikirim ke struct Server
 	logMiddleware := &LogMiddleware{
 		Handler: mux,
+	}
+	errorHandler := &ErrorHandler{
+		Handler: logMiddleware,
 	}
 
 	server := http.Server{
 		Addr:    "localhost:8080",
-		Handler: logMiddleware,
+		Handler: errorHandler,
 	}
 	err := server.ListenAndServe()
 	if err != nil {

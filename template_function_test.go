aec582fcd48abcb633f9a0d5d0c94a4e52ec6ab1
Muhammad Deril <49135753+derilkillms@users.noreply.github.com>
2023-03-13 10:46:39 +0700

    web

--- template_function_test.go
+++ template_function_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"text/template"
 )
@@ -26,6 +27,71 @@ func TestTemplateFunction(t *testing.T) {
 	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
 	recorder := httptest.NewRecorder()
 	TemplateFunction(recorder, request)
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+// Golang template memiliki beberapa global function, global function adalah function yang bisa digunakan secara langsung, tanpa menggunakan template data. Berikut adalah beberapa global function di golang template : https://github.com/golang/go/blob/master/src/text/template/funcs.go
+
+//  kita bisa menggunakan method Funcs pada template. Tetapi perlu di ingat, bahwa menambahkan global function harus dilakukan sebelum parsing template.
+
+func TemplateFunctionGlobal(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.New("FUNCTION").Parse(`{{len .Name}}`))
+	t.ExecuteTemplate(writer, "FUNCTION", MyPage{
+		Name: "Deril",
+	})
+}
+
+func TestTemplateFunctionGlobal(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+	TemplateFunctionGlobal(recorder, request)
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TemplateCreateGlobal(writer http.ResponseWriter, request *http.Request) {
+	t := template.New("FUNCTION")
+	t = t.Funcs(map[string]interface{}{
+		"upper": func(value string) string {
+			return strings.ToUpper(value)
+		},
+	})
+	t = template.Must(t.Parse(`{{upper .Name}}`))
+
+	t.ExecuteTemplate(writer, "FUNCTION", MyPage{
+		Name: "Deril",
+	})
+}
+func TestTemplateCreateGlobal(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+	TemplateCreateGlobal(recorder, request)
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TemplateCreateGlobalPipelines(writer http.ResponseWriter, request *http.Request) {
+	t := template.New("FUNCTION")
+	t = t.Funcs(map[string]interface{}{
+		"sayHello": func(name string) string {
+			return "Hello " + name
+		},
+		"upper": func(value string) string {
+			return strings.ToUpper(value)
+		},
+	})
+	t = template.Must(t.Parse(`{{sayHello .Name | upper}}`))
+	t.ExecuteTemplate(writer, "FUNCTION", MyPage{
+		Name: "Deril",
+	})
+}
+
+func TestCreateGlobalPipelines(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateCreateGlobalPipelines(recorder, request)
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
 }
